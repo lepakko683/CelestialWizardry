47ce64ecc31a91f6932defbdc963d3ce393d6800
PizzAna <anttikiviyo@gmail.com>
2014-05-02 23:41:15 +0300

    Added sided textures for writing table

--- src/main/java/celestialwizardry/block/BlockWritingTable.java
+++ src/main/java/celestialwizardry/block/BlockWritingTable.java
@@ -1,18 +1,61 @@
 package celestialwizardry.block;
 
 import celestialwizardry.reference.Names;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
 
 public class BlockWritingTable extends BlockCW implements ITileEntityProvider
 {
+    @SideOnly(Side.CLIENT)
+    private IIcon sideIcon;
+
+    @SideOnly(Side.CLIENT)
+    private IIcon topIcon;
+
+    @SideOnly(Side.CLIENT)
+    private IIcon bottomIcon;
+
     public BlockWritingTable()
     {
         super(Material.wood);
         this.setHardness(2.0f);
         this.setBlockName(Names.Blocks.WRITING_TABLE);
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void registerBlockIcons(IIconRegister iconRegister)
+    {
+        sideIcon = iconRegister.registerIcon(
+                String.format("%s_%s", getUnwrappedUnlocalizedName(this.getUnlocalizedName()), "side"));
+        topIcon = iconRegister.registerIcon(
+                String.format("%s_%s", getUnwrappedUnlocalizedName(this.getUnlocalizedName()), "top"));
+        bottomIcon = iconRegister.registerIcon(
+                String.format("%s_%s", getUnwrappedUnlocalizedName(this.getUnlocalizedName()), "bottom"));
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public IIcon getIcon(int side, int meta)
+    {
+        if (side == 0)
+        {
+            return bottomIcon;
+        }
+        else if (side == 1)
+        {
+            return topIcon;
+        }
+        else
+        {
+            return sideIcon;
+        }
     }
 
     /**
