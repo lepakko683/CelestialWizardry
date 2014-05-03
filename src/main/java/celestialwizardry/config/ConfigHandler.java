15b430b9942ab85130f3019f0dc6acf383455fe3
PizzAna <anttikiviyo@gmail.com>
2014-05-03 12:10:16 +0300

    Added recipes

--- src/main/java/celestialwizardry/config/ConfigHandler.java
+++ src/main/java/celestialwizardry/config/ConfigHandler.java
@@ -1,9 +1,11 @@
 package celestialwizardry.config;
 
+import java.io.File;
+
 public class ConfigHandler
 {
     public static void init(String configPath)
     {
-
+        ClientConfig.init(new File(configPath + "client.cfg"));
     }
 }
--- src/main/java/celestialwizardry/item/crafting/RecipesVanilla.java
+++ src/main/java/celestialwizardry/item/crafting/RecipesVanilla.java
@@ -1,9 +1,44 @@
 package celestialwizardry.item.crafting;
 
+import celestialwizardry.init.ModBlocks;
+import celestialwizardry.init.ModItems;
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.oredict.ShapedOreRecipe;
+import net.minecraftforge.oredict.ShapelessOreRecipe;
+
 public class RecipesVanilla
 {
     public static void init()
     {
+        // Initialize block crafting recipes
+        initBlockRecipes();
 
+        // Initialize item crafting recipes
+        initItemRecipes();
+    }
+
+    private static void initBlockRecipes()
+    {
+        // Writing table
+        add(new ItemStack(ModBlocks.writingTable), " l ", "bpb", "s s", 'l', Items.leather, 'b', "slabWood", 'p',
+            "plankWood", 's', "stairWood");
+    }
+
+    private static void initItemRecipes()
+    {
+        // Scroll
+        add(new ItemStack(ModItems.scroll), "p", "p", "p", 'p', Items.paper);
+    }
+
+    private static void add(ItemStack output, Object... input)
+    {
+        GameRegistry.addRecipe(new ShapedOreRecipe(output, input));
+    }
+
+    private static void addShapeless(ItemStack output, Object... input)
+    {
+        GameRegistry.addRecipe(new ShapelessOreRecipe(output, input));
     }
 }
