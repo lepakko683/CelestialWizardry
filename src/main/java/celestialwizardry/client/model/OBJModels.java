898cf94428b4f9599386f6cd409315320c22f656
le683 <lepakko683@gmail.com>
2014-05-28 15:32:43 +0300

    I hope this works now

--- src/main/java/celestialwizardry/client/model/OBJModels.java
+++ src/main/java/celestialwizardry/client/model/OBJModels.java
@@ -6,7 +6,7 @@ import net.minecraftforge.client.model.obj.WavefrontObject;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
-public class OBJmodels
+public class OBJModels
 {
 
     public static WavefrontObject modelWritingTable;
