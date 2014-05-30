900d52255346a3fba27fdcbf9f00cbc930f49263
PizzAna <anttikiviyo@gmail.com>
2014-05-30 12:10:48 +0300

    :D

--- src/main/java/celestialwizardry/item/ItemMatrix.java
+++ src/main/java/celestialwizardry/item/ItemMatrix.java
@@ -1,6 +1,11 @@
 package celestialwizardry.item;
 
 import celestialwizardry.reference.Names;
+import celestialwizardry.util.StringHelper;
+
+import net.minecraft.item.ItemStack;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 public class ItemMatrix extends ItemCW
 {
@@ -8,5 +13,13 @@ public class ItemMatrix extends ItemCW
     {
         super();
         this.setUnlocalizedName(Names.Items.MATRIX);
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public String getItemStackDisplayName(ItemStack stack)
+    {
+        return StringHelper.LIGHT_BLUE + super.getItemStackDisplayName(stack) + StringHelper.END + StringHelper.RED
+                + StringHelper.localize("TODO") + StringHelper.END;
     }
 }
