aaed088156c3be31317cf71f2c553e25b7e2ac76
PizzAna <anttikiviyo@gmail.com>
2014-05-30 11:23:01 +0300

    Fixed a server crash

--- src/main/java/celestialwizardry/client/handler/ClientTickEventHandler.java
+++ src/main/java/celestialwizardry/client/handler/ClientTickEventHandler.java
@@ -10,7 +10,6 @@ import cpw.mods.fml.common.gameevent.TickEvent;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
-@SideOnly(Side.CLIENT)
 public class ClientTickEventHandler
 {
     public static int ticksWithBookOpen = 0;
