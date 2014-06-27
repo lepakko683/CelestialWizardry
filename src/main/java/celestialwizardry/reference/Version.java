a127ea03314d0a3f8b53fae8a3a509ef646acfe4
PizzAna <anttikiviyo@gmail.com>
2014-06-27 17:23:26 +0300

    Just DEV

--- src/main/java/celestialwizardry/reference/Version.java
+++ src/main/java/celestialwizardry/reference/Version.java
@@ -6,7 +6,7 @@ package celestialwizardry.reference;
 public class Version
 {
     // TODO Remove System.getenv() (potential NPE)
-    public static final String VERSION = "0.1-DEV.${System.getenv().BUILD_NUMBER}";
+    public static final String VERSION = "0.1-DEV";
     public static final String FORGE = "10.12.2.1121";
     public static final String MINECRAFT = "1.7.2";
     public static final String BAUBLES = "1.0.0.16";
