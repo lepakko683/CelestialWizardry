71f4369140bf5bf9e83ba9b15bb1404ef150de44
PizzAna <anttikiviyo@gmail.com>
2014-07-09 18:21:55 +0300

    No need to load this yet

--- src/main/java/celestibytes/core/CelestiCore.java
+++ src/main/java/celestibytes/core/CelestiCore.java
@@ -4,7 +4,7 @@ import cpw.mods.fml.common.Mod;
 
 import celestibytes.core.reference.Reference;
 
-@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION, certificateFingerprint = Reference.FINGERPRINT, dependencies = Reference.DEPENDENCIES /* , guiFactory = Reference.GUI_FACTORY_CLASS */)
+// @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION, certificateFingerprint = Reference.FINGERPRINT, dependencies = Reference.DEPENDENCIES /* , guiFactory = Reference.GUI_FACTORY_CLASS */)
 public class CelestiCore
 {
 }
