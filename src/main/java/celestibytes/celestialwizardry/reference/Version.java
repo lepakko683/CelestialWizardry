de74e3eae9ee6242acaa57c1c259ec899d1e79b9
PizzAna <anttikiviyo@gmail.com>
2014-07-16 02:19:41 +0300

    Minor

--- src/main/java/celestibytes/celestialwizardry/reference/Version.java
+++ src/main/java/celestibytes/celestialwizardry/reference/Version.java
@@ -5,8 +5,8 @@ import celestibytes.core.mod.version.ModVersion;
 
 public class Version
 {
-    public static final String VERSION = "0.1.0";
-    public static final ModVersion MOD_VERSION = new ModVersion(0, 1, 0);
+    public static final String VERSION = "0.1.0-alpha.1";
+    public static final ModVersion MOD_VERSION = new ModVersion(0, 1, 0, Channel.ALPHA, 1);
     public static final String FORGE = "10.13.0.1180";
     public static final String MINECRAFT = "1.7.10";
     public static final String BAUBLES = "1.0.0.16";
