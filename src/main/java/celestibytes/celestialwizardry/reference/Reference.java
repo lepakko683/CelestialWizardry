47558155250e4aabdda8362f68f8c8c61b8b19cd
PizzAna <anttikiviyo@gmail.com>
2014-07-15 11:39:18 +0300

    Fix the config gui

--- src/main/java/celestibytes/celestialwizardry/reference/Reference.java
+++ src/main/java/celestibytes/celestialwizardry/reference/Reference.java
@@ -14,7 +14,7 @@ public class Reference
     public static final String SERVER_PROXY_CLASS = "celestibytes.celestialwizardry.proxy.ServerProxy";
     public static final String CLIENT_PROXY_CLASS = "celestibytes.celestialwizardry.proxy.ClientProxy";
 
-    public static final String GUI_FACTORY_CLASS = "celestialwizardry.config.gui.CWGuiFactory";
+    public static final String GUI_FACTORY_CLASS = "celestibytes.celestialwizardry.config.gui.CWGuiFactory";
 
     public static final String FINGERPRINT = "@FINGERPRINT@";
 
